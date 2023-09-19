38eebdd023c0f1d356b792d8983ce67ffc574bf0
chyupapimunyanya <144160700+chyupapimunyanya@users.noreply.github.com>
2023-09-19 15:41:26 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -25,34 +25,5 @@ fun calculateFullName2(firstName : String, lastName : String): Pair<String, Int>
 }
 //-------------------------
 
-//Задача 5
-    fun primeNumber(n: Int):Boolean{
-    if (n < 2) return false
-        for (i in 2..n - 1) {
-        if (n % i == 0) return false
-    }
-    return true
-    }
-    println(primeNumber(4))
-//--------------------
-
-//Задача 6
-    fun fibonacci(n: Int) : Int{
-        var num1 = 0
-        var num2 = 1
-        var num3: Int
-    if (n == 0) return 0
-        else if (n == 1) return 1
-    else
-        for (i in 2 until n+1)
-        {
-            num3 = num1 + num2
-            num1 = num2
-            num2 = num3
-            }
-        return num2
-    }
-    println(fibonacci(9))
-//---------------------
 }
 
